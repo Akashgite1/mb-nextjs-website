9e9ba47a411275fe2bdf0a34de64f46807f57436
Akashgite1 <akashgite550@gmail.com>
2025-10-04 16:09:05 +0530

    refactor: update JavaScript topics and links for clarity and consistency

--- src/components/roadmapSections/TechData.ts
+++ src/components/roadmapSections/TechData.ts
@@ -3,37 +3,94 @@ export const topics = [
         title: "Javascript",
         rowData: [
             {
-                title: "Output based Ques #1",
+                title: "JS - Output based Ques #1",
                 links: [
                     {
-                        label: "Questions",
-                        url: "https://medium.com/@contactmanoharbatra/js-reverse-a-string-1fb6539e0597",
-                    },
-                    {
-                        label: "Questions",
+                        label: "Article",
                         url: "https://medium.com/@contactmanoharbatra/js-reverse-a-string-1fb6539e0597",
                     },
                 ],
             },
             {
-                title: "Output based Ques #2",
+                title: "JS - Output based Ques #2",
                 links: [
                     {
-                        label: "Questions",
+                        label: "Article",
                         url: "https://medium.com/@contactmanoharbatra/js-output-based-ques-2-857ffb35b9e1",
                     },
                 ],
-                // rowData: [
-                //     {
-                //         title: "Arrow",
-                //         links: [
-                //             {
-                //                 label: "Social Media is New Resume",
-                //                 url: "https://www.youtube.com/watch?v=JdzYJZTLrxo",
-                //             },
-                //         ],
-                //     },
-                // ],
+            },
+            {
+                title: "JS ES6 — Map, Filter and Reduce",
+                links: [
+                    {
+                        label: "Article",
+                        url: "https://medium.com/@contactmanoharbatra/js-es6-map-filter-and-reduce-4c76ba48124b",
+                    },
+                ],
+            },
+            {
+                title: "JS Basics",
+                links: [
+                    {
+                        label: "Article",
+                        url: "https://medium.com/@contactmanoharbatra/js-basics-b944cdbafdab",
+                    },
+                ],
+            },
+            {
+                title: "Javascript Helper Functions",
+                links: [
+                    {
+                        label: "Article",
+                        url: "https://medium.com/@contactmanoharbatra/javascript-helper-functions-43a3f70ae6e6",
+                    },
+                ],
+            },
+            {
+                title: "JS — Closures",
+                links: [
+                    {
+                        label: "Article",
+                        url: "https://medium.com/@contactmanoharbatra/js-closures-8f9101a4a6ea",
+                    },
+                ],
+            },
+            {
+                title: "JS — Split and Splice",
+                links: [
+                    {
+                        label: "Article",
+                        url: "https://medium.com/@contactmanoharbatra/js-split-and-splice-e9faef55f818",
+                    },
+                ],
+            },
+            {
+                title: "JS — HOC & Custom Hooks",
+                links: [
+                    {
+                        label: "Article",
+                        url: "https://medium.com/@contactmanoharbatra/js-hoc-custom-hooks-fd5f4661e674",
+                    },
+                ],
+            },
+            {
+                title: "JS - Generator Functions vs Async/Await",
+                links: [
+                    {
+                        label: "Article",
+                        url: "https://medium.com/@contactmanoharbatra/generator-functions-vs-async-await-24270dc7bee0",
+                    },
+                ],
+            },
+            {
+                title: "JS - Deep Copy & Shallow Copy",
+                links: [
+                    {
+                        label: "Article",
+                        url: "https://medium.com/@contactmanoharbatra/js-deep-copy-vs-shallow-copy-4b88118e059e",
+                    },
+                ],
             },
         ],
     },
@@ -42,25 +99,85 @@ export const topics = [
         rowData: [
             {
                 title: "Everything about ReactJs",
+                links: [
+                    {
+                        label: "Article",
+                        url: "https://medium.com/@contactmanoharbatra/everything-about-reactjs-8bc8305b3036",
+                    },
+                ],
             },
             {
-                title: "Portals in React",
+                title: "What Are Portals in React?",
+                links: [
+                    {
+                        label: "Article",
+                        url: "https://medium.com/@contactmanoharbatra/what-are-portals-in-react-aa9600b9c485",
+                    },
+                ],
             },
             {
                 title: "Middlewares in React",
-            },
-            {
-                title: "ReactJS — Find the mistake in code",
-            },
-            {
-                title: "Interview Questions #1",
                 links: [
                     {
-                        label: "Article 1",
+                        label: "Article",
+                        url: "https://medium.com/@contactmanoharbatra/middlewares-in-react-3e4391c7f7cf",
+                    },
+                ],
+            },
+            {
+                title: "ReactJS — Find the mistake in code",
+                links: [
+                    {
+                        label: "Article",
+                        url: "https://medium.com/@contactmanoharbatra/reactjs-find-the-mistake-in-code-0acf30a0200f",
+                    },
+                ],
+            },
+            {
+                title: "React Interview Questions #1",
+                links: [
+                    {
+                        label: "Article",
                         url: "https://medium.com/@contactmanoharbatra/react-interview-questions-1-eb71bee6443b",
+                    },
+                ],
+            },
+            {
+                title: "Crack React Interview Easily",
+                links: [
+                    {
+                        label: "Article",
+                        url: "https://medium.com/@contactmanoharbatra/crack-react-interview-easily-47cabcd8be22",
+                    },
+                ],
+            },
+            {
+                title: "ReactJS Optimized Code",
+                links: [
+                    {
+                        label: "Article",
+                        url: "https://medium.com/@contactmanoharbatra/reactjs-optimized-code-358d98e3359f",
+                    },
+                ],
+            },
+            {
+                title: "React Hooks",
+                links: [
+                    {
+                        label: "Article",
+                        url: "https://medium.com/@contactmanoharbatra/react-hooks-54efff9ea288",
+                    },
+                ],
+            },
+            {
+                title: "Redux Toolkit vs UseContext",
+                links: [
+                    {
+                        label: "Article",
+                        url: "https://medium.com/@contactmanoharbatra/redux-toolkit-vs-usecontext-9b7ff23052f0",
                     },
                 ],
             },
         ],
     },
-]
+];
