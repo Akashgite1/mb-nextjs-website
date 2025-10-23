5dce5c9cb0652f171815b849cd192cd5b527f3d4
Manohar Batra <contactmanoharbatra@gmail.com>
2025-10-24 00:44:51 +0530

    blogs added

--- src/app/blogs/blogsData.ts
+++ src/app/blogs/blogsData.ts
@@ -2,17 +2,36 @@ import { IblogsData } from "./types";
 
 export const blogsData: IblogsData[]  = [
   {
+    mainHeader: "Jobs and Career",
+    externalLinks: [
+    ],
+    rowData: [
+      {
+        company: "One List for Job Seekers",
+        link: "https://medium.com/@contactmanoharbatra/one-list-for-job-seekers-1465739a0e1f",
+      },
+    ],
+  },
+  {
     mainHeader: "Frontend",
     externalLinks: [
     ],
     rowData: [
       {
-        company: "Nagarro Interview Questions",
-        link: "https://medium.com/1",
+        company: "One Sheet Javascript Interview Questions/Topics",
+        link: "https://medium.com/@contactmanoharbatra/javascript-interview-questions-topics-351d85bc29b0",
       },
       {
-        company: "Deloitte Interview Questions",
-        link: "https://medium.com/2",
+        company: "VISA Interview Questions",
+        link: "https://medium.com/@contactmanoharbatra/visa-frontend-interview-ques-a5c29e6c01a7",
+      },
+      {
+        company: "#1 Interview Questions",
+        link: "https://medium.com/@contactmanoharbatra/react-interview-questions-1-eb71bee6443b",
+      },
+      {
+        company: "#2 Interview Questions",
+        link: "https://medium.com/@contactmanoharbatra/reactjs-interview-questions-91c720acd9db",
       },
     ],
   },
@@ -23,8 +42,19 @@ export const blogsData: IblogsData[]  = [
     ],
     rowData: [
       {
-        company: "Accenture Interview Questions",
-        link: "https://medium.com/3",
+        company: "JWT - JSON Web Token",
+        link: "https://medium.com/@contactmanoharbatra/jwt-f876287f18e6",
+      },
+    ],
+  },
+  {
+    mainHeader: "System Design",
+    externalLinks: [
+    ],
+    rowData: [
+      {
+        company: "#1 Interview Questions",
+        link: "https://medium.com/@contactmanoharbatra/system-design-ques-b739f7438f4d",
       },
     ],
   },
