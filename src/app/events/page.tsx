b65e23e346f9ccc63d4550244a5dd831875f09b7
Manohar Batra <contactmanoharbatra@gmail.com>
2026-04-02 01:32:02 +0530

    added events

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -63,7 +63,7 @@ export default function EventsPage() {
 
   return (
     <div className="container mx-auto px-4 py-8">
-      <h1 className="text-3xl font-bold mb-6">Events (Past Events are found in Job Switch Course)</h1>
+      <h1 className="text-3xl font-bold mb-6">Events (Past Events recordings are found in Job Switch Course)</h1>
 
       {/* Tabs */}
       <div className="flex space-x-4 mb-6">
