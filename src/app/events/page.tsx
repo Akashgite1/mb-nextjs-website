dfaf79bfa90695ac6102da4718b0aa70ca45ba70
Manohar Batra <contactmanoharbatra@gmail.com>
2026-04-16 22:28:50 +0530

    event

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -20,16 +20,28 @@ type Event = {
 const events: Event[] = [
     {
         id: 3,
-        title: "Masterclass #3 - Career Growth & Corporate Politics",
+        title: "Masterclass #3 - Job Security after 30",
         description:
-            "Exploring career growth opportunities and corporate politics in the technology industry.",
-        image: "/images/events/corporateGrowth.png",
+            "Exploring job security and career stability for professionals in their 30s and beyond.",
+        image: "/images/events/jobsecurityafter30.png",
         speaker: "Manohar Batra",
-        date: "2026-04-11",
+        date: "2026-04-18",
         time: "11:00 AM",
         registrations: 0,
-        link: "https://topmate.io/manohar",
+        link: "https://topmate.io/manohar/2050710",
     },
+    // {
+    //     id: 3,
+    //     title: "Masterclass #3 - Career Growth & Corporate Politics",
+    //     description:
+    //         "Exploring career growth opportunities and corporate politics in the technology industry.",
+    //     image: "/images/events/corporateGrowth.png",
+    //     speaker: "Manohar Batra",
+    //     date: "2026-04-11",
+    //     time: "11:00 AM",
+    //     registrations: 0,
+    //     link: "https://topmate.io/manohar",
+    // },
     {
         id: 2,
         title: "Masterclass #2 - Impact of AI on IT Jobs",
